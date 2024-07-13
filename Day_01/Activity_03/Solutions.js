e7934558a5a3f03b27af22f3e429b0be53b86fd5
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-13 15:23:01 +0000

    Day1 Continued - Task4

---
+++ Day_01/Activity_03/Solutions.js
@@ -0,0 +1,12 @@
+// Solution to Task-4:
+
+const MyName = "Shubhashish Chakraborty";
+let MyAge = 69;
+let MaritalStatus = false;
+let Languages = [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "GoLang",
+    //Continue
+]
---
+++ Day_01/Activity_03/tasks.md
@@ -0,0 +1,3 @@
+# Activity_03: Data Types:
+
+### Task 4: Create variables of different Data types `(number , string , boolean , object , array)` and log each variable's type using the `typeof` Operator.

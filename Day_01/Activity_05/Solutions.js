2a1770d1cb4f53af40242a1ff198ec2f5862f1d7
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-13 18:18:38 +0000

    Day-1 Completed!!!

---
+++ Day_01/Activity_05/Solutions.js
@@ -0,0 +1,20 @@
+// Solution to Task-4:
+
+
+const changeMe = 34;
+
+changeMe = 23;
+
+// console.log(changeMe)
+
+// Obviuosly Its ERRRRRORRRR!!!
+
+/*
+
+    changeMe = 23;
+         ^
+
+    TypeError: Assignment to constant variable.
+
+
+*/
---
+++ Day_01/Activity_05/tasks.md
@@ -0,0 +1,3 @@
+# Activity_05: Understanding `const`:
+
+### Task 6: Try reassigning a variable declared with `const` and observe the error.

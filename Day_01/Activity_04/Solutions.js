9d3b3780b87963d6a423a0620b2e078327a794da
UNacc Shubh <154262783+KratosHere@users.noreply.github.com>
2024-07-13 18:11:38 +0000

    Activity-4 done

---
+++ Day_01/Activity_04/Solutions.js
@@ -0,0 +1,7 @@
+// Solution to Task-5:
+
+let myAge = 23; // My age in 2024
+console.log(`I was ${myAge} years old in 2024`);
+
+myAge = 26; // My age in 2027
+console.log(`I am ${myAge} years old in 2027`);
---
+++ Day_01/Activity_04/tasks.md
@@ -0,0 +1,3 @@
+# Activity_04: Reassigning Variables:
+
+### Task 5: Declare a variable using `let`, assign it a initial value, reassign a new value, and log both the values to the console.
